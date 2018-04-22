3b316109ff5ef58cca82bf727c2c1667d5dcf72c
MaryamAziz2 <maryam.uet300300@gmail.com>
2018-04-22 16:05:41 -0700

    files

---
+++ WcfService1/Book.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class Book
+    {
+        private string name;
+        private int price;
+        private int quantity;
+        private string location;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+
+            set
+            {
+                location = value;
+            }
+        }
+    }
+}
---
+++ WcfService1/Bookdl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class Bookdl
+    {
+        public static List<Book> Library = new List<Book>();
+        public static List<Book> Market = new List<Book>();
+        public static void AddBookToLib(Book b)
+        {
+            
+        }
+        public static void AddBookToMar(Book b)
+        {
+
+        }
+        public static void UpdatePrice(Book b)
+        {
+
+        }
+        public static void UpdateQuantity(Book b)
+        {
+
+        }
+        public static void DeleteBookFromLib(Book b)
+        {
+
+        }
+        public static void DeleteBookFromMar(Book b)
+        {
+
+        }
+        public static List<Book> SearchBookFromLibrary(string name)
+        {
+            return null;
+        }
+        public static List<Book> SearchBookFromMar(string name)
+        {
+            return null;
+        }
+    }
+}
