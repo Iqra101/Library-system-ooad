9973e46536937edace0558d47501de6dd60fe959
Aqsa Abid <abid.aqsa26@gmail.com>
2018-05-11 10:53:36 +0500

    rider

---
+++ WindowsFormsApplication14/Rider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication14
+{
+    public partial class Rider : Form
+    {
+        public Rider()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Okay:(");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("you'll be provided with the rider:)");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Search_From_Market s = new Search_From_Market();
+            this.Hide();
+            s.Show();
+        }
+    }
+}
